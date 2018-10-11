4045c09f3ab19393d95fa59ddc071af58a2d4bf3
Fab <fab.pomata@gmail.com>
2018-10-11 15:00:27 +0200

    [WIP] menu builder

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,5 +1,59 @@
 const { app, Menu } = require('electron')
 
+export default class menuBuilder {
+  constructor(mainWindow) {
+    this.mainWindow = mainWindow
+  }
+
+  init() {
+    if (
+      process.env.NODE_ENV === 'development' ||
+      process.env.DEBUG_PROD === 'true'
+    ) {
+      this.setupDevEnvironment()
+    }
+    const template = this.buildMenu()
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+  }
+
+  setupDevEnvironment() {
+    this.mainWindow.openDevTools()
+    this.mainWindow.webContents.on('context-menu', (e, props) => {
+      const { x, y } = props
+
+      Menu.buildFromTemplate([
+        {
+          label: 'Inspect element',
+          click: () => {
+            this.mainWindow.inspectElement(x, y)
+          },
+        },
+      ]).popup(this.mainWindow)
+    })
+  }
+
+  buildMenu() {
+    const template = [
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Cmd+R',
+            role: 'reload',
+          },
+        ],
+      },
+      {
+        role: 'window',
+        submenu: [{ role: 'minimize' }, { role: 'close' }],
+      },
+    ]
+  }
+}
+
 const template = [
   {
     label: 'Edit',
@@ -12,9 +66,9 @@ const template = [
       { role: 'paste' },
       { role: 'pasteandmatchstyle' },
       { role: 'delete' },
-      { role: 'selectall' }
-    ]
-  }
+      { role: 'selectall' },
+    ],
+  },
 ]
 
 if (process.platform === 'darwin') {
@@ -28,10 +82,7 @@ if (global.debug.devTools === 1) {
       label: 'Open Dev-Tools',
       click: (item, focusedWindow) => {
         if (focusedWindow) focusedWindow.toggleDevTools()
-      }
-    }
+      },
+    },
   )
 }
-
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
