646a07bb12f65d0cf7371eac7a45d682c0ad9169
Fabrice_Pomata <fab.pomata@gmail.com>
2018-10-16 18:50:42 +0200

    [wip] trying to force a html template

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -3,6 +3,9 @@ import { ipcRenderer } from 'electron'
 import '../assets/shrinkit.css'
 
 const app = document.getElementById('app')
+const dragArea = document.createElement('div')
+dragArea.setAttribute('id', 'drag-area')
+app.appendChild(dragArea)
 
 app.onclick = e => {
   console.log(e)
--- webpack/webpack.config.overload.js
+++ webpack/webpack.config.overload.js
@@ -1,10 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ShrinktIt',
+      template: path.resolve(__dirname, '../src', 'index.html'),
+      hash: true,
+    }),
+  ],
 }
