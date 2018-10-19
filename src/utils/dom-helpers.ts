5dc2443f52d73703ee539115bac12cc4ce4962ee
Fabrice_Pomata <fab.pomata@gmail.com>
2018-10-19 20:18:43 +0200

    make createdDiv function more generic and re usable

--- src/utils/dom-helpers.ts
+++ src/utils/dom-helpers.ts
@@ -1,30 +1,30 @@
 import { formatBytes } from '../utils/formatters'
 
-const createDiv = () => document.createElement('div')
+const createEl = (el: string) => document.createElement(el)
 const createText = (content: string) => document.createTextNode(content)
 
 export const createFileList = (fileName: string, originalSize: number) => {
   // Status
-  const statusEl: HTMLElement = createDiv()
+  const statusEl: HTMLElement = createEl('div')
   statusEl.setAttribute('class', 'status-icon col-1 p-sm text-center')
   statusEl.appendChild(createText('✓'))
 
   // File Name
-  const fileNameEl: HTMLElement = createDiv()
+  const fileNameEl: HTMLElement = createEl('div')
   fileNameEl.setAttribute('class', 'file-name col-5 p-sm')
   fileNameEl.appendChild(createText(fileName))
 
   // Original Size
-  const originalSizeEl: HTMLElement = createDiv()
+  const originalSizeEl: HTMLElement = createEl('div')
   originalSizeEl.setAttribute('class', 'file-orig-size col-2 p-sm text-right')
   originalSizeEl.appendChild(createText(formatBytes(originalSize)))
 
   // Compressed Size
-  const compressedSizeEl: HTMLElement = createDiv()
+  const compressedSizeEl: HTMLElement = createEl('div')
   compressedSizeEl.setAttribute('class', 'file-new-size col-2 p-sm text-right')
 
   // Saved
-  const savedEl: HTMLElement = createDiv()
+  const savedEl: HTMLElement = createEl('div')
   savedEl.setAttribute('class', 'savings col-2 p-sm text-right')
 
   const fragment = document.createDocumentFragment()
