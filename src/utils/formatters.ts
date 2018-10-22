815d07c21137b6f0a0265f569273bd9a549cd8d3
Fab <fab.pomata@gmail.com>
2018-10-22 08:57:26 +0200

    changed bytes display to lowercase

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -8,7 +8,7 @@ export const formatBytes: (bytes: number, decimals?: number) => string = (bytes:
 
   const dm = decimals <= 0 ? 0 : decimals || 2
 
-  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const units = ['Bytes', 'kb', 'mb', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
   const i = Math.floor(Math.log(bytes) / Math.log(k))
   return `${parseFloat((bytes / k ** i).toFixed(dm))} ${units[i]}`
